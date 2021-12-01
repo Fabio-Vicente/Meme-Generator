8b48f18c1a6fbe6dc524824be0bbe5ea834bfb20
Fábio Vicente <fab10_lima@hotmail.com>
2021-11-30 22:59:52 -0300

    Requisito 2: Efetuando o upload de uma imagem para o meme

--- script.js
+++ script.js
@@ -1,8 +1,20 @@
 const textInput = document.getElementById('text-input');
+const imgInput = document.getElementById('meme-insert');
 const memeText = document.getElementById('meme-text');
+const memeImg = document.getElementById('meme-image');
 
 function transcryptText() {
   memeText.innerText = textInput.value;
 }
+//  source: https://stackoverflow.com/questions/4459379/preview-an-image-before-it-is-uploaded/27165977#27165977
+function importImg(loadImg) {
+  const img = loadImg.target;
+  memeImg.src = URL.createObjectURL(img.files[0]);
+}
+function freeImgMemory(image) {
+  URL.revokeObjectURL(image.src);
+}
 
 textInput.onkeyup = transcryptText;
+imgInput.onchange = importImg;
+memeImg.onload = freeImgMemory;
