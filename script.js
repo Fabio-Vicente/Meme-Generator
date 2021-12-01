00672574bdc77f0bdab76db27dccadfa2e4adfd8
Fábio Vicente <fab10_lima@hotmail.com>
2021-12-01 02:12:01 -0300

    Requisito 6: Adicionando botões elementais

--- script.js
+++ script.js
@@ -2,6 +2,10 @@ const textInput = document.getElementById('text-input');
 const imgInput = document.getElementById('meme-insert');
 const memeText = document.getElementById('meme-text');
 const memeImg = document.getElementById('meme-image');
+const container = document.getElementById('meme-image-container');
+const fireButton = document.getElementById('fire');
+const waterButton = document.getElementById('water');
+const earthButton = document.getElementById('earth');
 
 function transcryptText() {
   memeText.innerText = textInput.value;
@@ -14,7 +18,14 @@ function importImg(loadImg) {
 function freeImgMemory(image) {
   URL.revokeObjectURL(image.src);
 }
+function newBorder(click) {
+  const borderButton = click.target;
+  container.className = borderButton.id;
+}
 
 textInput.onkeyup = transcryptText;
 imgInput.onchange = importImg;
 memeImg.onload = freeImgMemory;
+fireButton.onclick = newBorder;
+waterButton.onclick = newBorder;
+earthButton.onclick = newBorder;
